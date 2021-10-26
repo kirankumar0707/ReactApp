5d2fafe737e2b0e98c63759970ce81ec18edc4a6
kirankumar0707 <44940009+kirankumar0707@users.noreply.github.com>
2021-10-26 23:57:17 +0530

    First commit. Hope it works

---
+++ Kiran_Kumar_A/build/precache-manifest.340e6eb9b4295bb0dbd6a646ff111b67.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfcd451b72098756b43a",
+    "url": "/static/js/main.dfcd451b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "357814436f44ca4dda4f",
+    "url": "/static/js/2.35781443.chunk.js"
+  },
+  {
+    "revision": "20dd33446ca05aca863ff568dc4d0f75",
+    "url": "/index.html"
+  }
+];
